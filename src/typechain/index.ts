6ee0862e1e52808c568d30aee8d94c746cddde28
Anton Adamansky <adamansky@gmail.com>
2021-12-08 23:26:48 +0700

    * Added ERC20MinterBurner solidity contract used by aiax-node

--- src/typechain/index.ts
+++ src/typechain/index.ts
@@ -5,6 +5,8 @@ export { Erc20Factory } from "./Erc20Factory";
 export type { Erc20 } from "./Erc20";
 export { CosmosErc20Factory } from "./CosmosErc20Factory";
 export type { CosmosErc20 } from "./CosmosErc20";
+export { Erc20MinterBurnerFactory } from "./Erc20MinterBurnerFactory";
+export type { Erc20MinterBurner } from "./Erc20MinterBurner";
 export { Erc20TokenOneFactory } from "./Erc20TokenOneFactory";
 export type { Erc20TokenOne } from "./Erc20TokenOne";
 export { GravityFactory } from "./GravityFactory";
